5e3734c4905d417184049248fdf0e52f364099bc
Linghui Zeng <lh.zeng@hotmail.com>
2016-09-27 13:08:54 +0800

    Create wordPattern.py

---
+++ wordPattern.py
@@ -0,0 +1,15 @@
+def wordPattern(self, pattern, str):
+    s = pattern
+    t = str.split()
+    return map(s.find, s) == map(t.index, t)
+
+
+def wordPattern(self, pattern, str):
+    f = lambda s: map({}.setdefault, s, range(len(s)))
+    return f(pattern) == f(str.split())
+ 
+
+def wordPattern(self, pattern, str):
+    s = pattern
+    t = str.split()
+    return len(set(zip(s, t))) == len(set(s)) == len(set(t)) and len(s) == len(t)
