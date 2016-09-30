e889152900496dd35ab341a14db485d40599bb3f
Linghui Zeng <lh.zeng@hotmail.com>
2016-09-30 10:44:39 +0800

    Update isUgly.py

--- isUgly.py
+++ isUgly.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type num: int
         :rtype: bool
         """
-        for p in 2, 3, 5:
-            while num % p == 0 < num:
-                num /= p
+        for prime in range(2, 7)[::-1]:
+            while num % prime == 0 < num:
+                num /= prime
         return num == 1
